94a4a15122afab7802575c5f19f26d3f1ca241b6
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2022-12-03 17:40:10 -0300

    Add Item unit test

---
+++ src/test/java/com/example/waiterapp/Item/ItemTest.java
@@ -0,0 +1,34 @@
+
+package com.example.waiterapp.Item;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.example.waiterapp.item.Item;
+
+@DisplayName("Testes dos métodos da classe Item")
+public class ItemTest {
+  private static Item item;
+
+  public static Item criaItem(int id, String name, String descrição, double preco) {
+    LocalDateTime dataCriacao = LocalDateTime.now();
+    return new Item((long) id, name, descrição, dataCriacao, preco);
+  }
+
+  @BeforeAll
+  public static void inicializaItem() {
+    item = criaItem(1, "Batata", "Batata frita", 10.00);
+  }
+
+  @DisplayName("Item#equals deve ser falso com itens diferentes")
+  @Test
+  public void equals_ItensDiferentes_False() {
+    Item item2 = criaItem(2, "Abacaxi", "Abacaxi fatiado", 5.00);
+
+    assertFalse(item.equals(item2));
+  }
+}
