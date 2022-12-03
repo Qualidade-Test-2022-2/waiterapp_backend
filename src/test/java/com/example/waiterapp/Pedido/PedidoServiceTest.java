0e7ddb22f71b64dac39f595e7df884e34bb7c815
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2022-12-03 18:52:07 -0300

    Add Pedido and PedidoService unit tests

---
+++ src/test/java/com/example/waiterapp/Pedido/PedidoServiceTest.java
@@ -0,0 +1,152 @@
+package com.example.waiterapp.Pedido;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import com.example.waiterapp.pedido.Pedido;
+import com.example.waiterapp.pedido.PedidoDTO;
+import com.example.waiterapp.pedido.PedidoRepository;
+import com.example.waiterapp.pedido.PedidoService;
+import com.example.waiterapp.cliente.ClienteService;
+import com.example.waiterapp.exceptions.ObjectNotFoundException;
+import com.example.waiterapp.item.ItemService;
+import com.example.waiterapp.itempedido.ItemPedidoRepository;
+import com.example.waiterapp.pagamento.PagamentoRepository;
+
+@DisplayName("PedidoService's tests")
+class PedidoServiceTest {
+
+  static PedidoService pedidoService;
+
+  @Mock
+  Pedido pedido1 = mock(Pedido.class);
+  Pedido pedido2 = mock(Pedido.class);
+
+  static PedidoRepository pedidoRepository = mock(PedidoRepository.class);
+  static PagamentoRepository pagamentoRepository = mock(PagamentoRepository.class);
+  static ItemPedidoRepository itemPedidoRepository = mock(ItemPedidoRepository.class);
+  static ItemService itemService = mock(ItemService.class);
+  static ClienteService clienteService = mock(ClienteService.class);
+
+  @BeforeAll
+  public static void inicializaPedidoService() {
+    pedidoService = new PedidoService(pedidoRepository, pagamentoRepository, itemPedidoRepository, itemService, clienteService);
+  }
+
+  @Nested
+  @DisplayName("PedidoService#listaPedidos")
+  class ListaPedidosTest {
+    @BeforeEach
+    public void mockPedidoRepositoryFindAll() {
+      ArrayList<Pedido> pedidos = new ArrayList<Pedido>(List.of(pedido1, pedido2));
+      when(pedidoRepository.findAll()).thenReturn(pedidos);
+    }
+
+    @Test
+    @DisplayName("should return a list of pedidos")
+    public void returnListOfPedidos() {
+      assertEquals(pedidoService.listaPedidos().get(0), pedido1);
+      assertEquals(pedidoService.listaPedidos().get(1), pedido2);
+    }
+  }
+
+  @Nested
+  @DisplayName("PedidoService#transformarDTO")
+  class TransformarDTOTest {
+    @Mock
+    PedidoDTO pedidoDTO = mock(PedidoDTO.class);
+
+    @Test
+    @DisplayName("should transform a PedidoDTO into a Pedido")
+    public void transformPedidoDTOIntoPedido() {
+      Pedido pedidoTransformado = pedidoService.transformarDTO(pedidoDTO);
+
+      assertAll(
+        () -> assertEquals(pedidoTransformado.getId(), pedidoDTO.getId()),
+        () -> assertEquals(pedidoTransformado.getEstado(), pedidoDTO.getEstado()),
+        () -> assertEquals(pedidoTransformado.getDataCriacao(), pedidoDTO.getDataCriacao()),
+        () -> assertEquals(pedidoTransformado.getNotaAtendimento(), pedidoDTO.getNotaAtendimento()),
+        () -> assertEquals(pedidoTransformado.getNotaPedido(), pedidoDTO.getNotaPedido())
+      );
+    }
+  }
+
+  @Nested
+  @DisplayName("PedidoService#returnPedidoById")
+  class RetornaPedidoByIdTest {
+    @Test
+    @DisplayName("should return a pedido by id when it exists")
+    public void returnPedidoWhenPedidoExist() {
+      when(pedidoRepository.findById(anyLong())).thenReturn(java.util.Optional.of(pedido1));
+      assertEquals(pedidoService.retornaPedidoById(1L), pedido1);
+    }
+  }
+
+  @Nested
+  @DisplayName("PedidoService#inserePedido")
+  class InserePedidoTest {
+    @Test
+    @DisplayName("should insert a pedido")
+    public void insertPedido() {
+      when(pedidoRepository.save(pedido1)).thenReturn(pedido1);
+      assertEquals(pedidoService.inserePedido(pedido1), pedido1);
+    }
+  }
+
+  @Nested
+  @DisplayName("PedidoService#atualizaPedido")
+  class AtualizaPedidoTest {
+    @BeforeEach
+    public void mockPedidoRepositoryFindById() {
+      when(pedidoRepository.findById(anyLong())).thenReturn(java.util.Optional.of(pedido1));
+      when(pedidoRepository.save(pedido1)).thenReturn(pedido1);
+    }
+
+    @Test
+    @DisplayName("should update a pedido")
+    public void updatePedido() {
+      assertEquals(pedidoService.atualizaPedido(pedido1), pedido1);
+    }
+  }
+
+  @Nested
+  @DisplayName("PedidoService#apagaPedido")
+  class ApagaPedidoTest {
+    @BeforeEach
+    public void mockPedidoRepositoryFindById() {
+      when(pedidoRepository.findById(anyLong())).thenReturn(java.util.Optional.of(pedido1));
+    }
+
+    @Test
+    @DisplayName("should delete a pedido")
+    public void deletePedido() {
+      doNothing().when(pedidoRepository).deleteById(anyLong());
+      pedidoService.apagaPedido(1L);
+    }
+
+    @Test
+    @DisplayName("should delete a pedido")
+    public void throwExceptionWhenCouldNotDeletePedido() {
+      doThrow(DataIntegrityViolationException.class).when(pedidoRepository).deleteById(anyLong());
+
+      assertThrows(DataIntegrityViolationException.class, () -> pedidoService.apagaPedido(1L));
+    }
+  }
+}
---
+++ src/test/java/com/example/waiterapp/Pedido/PedidoTest.java
@@ -0,0 +1,58 @@
+
+package com.example.waiterapp.Pedido;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.example.waiterapp.pedido.Pedido;
+import com.example.waiterapp.enums.Estado;
+import com.example.waiterapp.itempedido.ItemPedido;
+import org.mockito.Mock;
+
+@DisplayName("Testes dos métodos da classe Pedido")
+public class PedidoTest {
+  private static Pedido pedido;
+
+  @Mock
+  static ItemPedido batata = mock(ItemPedido.class);
+  static ItemPedido tomate = mock(ItemPedido.class);
+  static ItemPedido guarana = mock(ItemPedido.class);
+
+  public static Pedido criaPedido(int id, String titulo, String descricao) {
+    LocalDateTime dataCriacao = LocalDateTime.now();
+    return new Pedido((long) id, dataCriacao, Estado.EM_PREPARACAO, 0.0, 1, 2, null);
+  }
+
+  @BeforeAll
+  public static void inicializaPedidoComItems() {
+    pedido = criaPedido(1, "Pedido com itens 1", "Pedido de teste");
+    Set<ItemPedido> items = new HashSet<ItemPedido>(Arrays.asList(batata, tomate));
+    pedido.setItems(items);
+  }
+
+  @DisplayName("Pedido#setPrecoTotal deve calcular o preço total do pedido")
+  @Disabled("must be implemented")
+  @Test
+  public void setPrecoTotalSomatoriaDosItens() {}
+
+  @DisplayName("Pedido#equals deve ser falso com pedidos diferentes")
+  @Test
+  public void equals_PedidosDiferentes_False() {
+    Pedido pedido2 = criaPedido(2, "Cardápio 2", "Pedido diferente");
+
+    assertFalse(pedido.equals(pedido2));
+  }
+}
