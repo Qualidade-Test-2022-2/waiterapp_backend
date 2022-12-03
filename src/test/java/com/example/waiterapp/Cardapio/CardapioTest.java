82b92edf2574f3c4e62dee547309e6069c475eff
Rafael Carrilho <rafaelcarrilho@id.uff.br>
2022-12-03 12:06:09 -0300

    Add Cardapio unit test

---
+++ src/test/java/com/example/waiterapp/Cardapio/CardapioTest.java
@@ -0,0 +1,48 @@
+
+package com.example.waiterapp.Cardapio;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.example.waiterapp.cardapio.Cardapio;
+import com.example.waiterapp.item.Item;
+import org.mockito.Mock;
+
+@DisplayName("Testes dos métodos da classe Cardapio")
+public class CardapioTest {
+  private static Cardapio cardapio;
+
+  @Mock
+  static Item batata = mock(Item.class);
+  static Item tomate = mock(Item.class);
+  static Item guarana = mock(Item.class);
+
+  public static Cardapio criaCardapio(int id, String titulo, String descricao) {
+    LocalDateTime dataCriacao = LocalDateTime.now();
+    return new Cardapio((long) id, dataCriacao, titulo, descricao);
+  }
+
+  @BeforeAll
+  public static void inicializaCardapioComItems() {
+    cardapio = criaCardapio(1, "Cardapio com itens 1", "Cardapio de teste");
+    List<Item> items = new ArrayList<>(Arrays.asList(batata, tomate));
+    cardapio.setItems(items);
+  }
+
+  @DisplayName("Cardapio#equals deve ser falso com cardapios diferentes")
+  @Test
+  public void equals_CardapiosDiferentes_False() {
+    Cardapio cardapio2 = criaCardapio(2, "Cardápio 2", "Cardapio diferente");
+
+    assertFalse(cardapio.equals(cardapio2));
+  }
+}
