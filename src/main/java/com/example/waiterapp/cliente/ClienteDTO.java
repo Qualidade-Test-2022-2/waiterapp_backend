99ba00021275ab285f7af3cfceca9cfe23d86f74
Alan Cavalcante <alanlimaemail@gmail.com>
2022-12-12 12:38:41 -0300

    chnage @NotNull to @NotBlank

--- src/main/java/com/example/waiterapp/cliente/ClienteDTO.java
+++ src/main/java/com/example/waiterapp/cliente/ClienteDTO.java
@@ -1,92 +1,50 @@
 package com.example.waiterapp.cliente;
 import com.example.waiterapp.pedido.Pedido;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-    @NotEmpty
+    @NotBlank
     @Size(min=3, message="The field must be at least {min} characters")
     @Size(max=100, message="The field must be maximium {max} characters")
     private String nome;
-    @NotEmpty
+    @NotBlank
     @Size(min=5, message="The field must be at least {min} characters")
     @Size(max=100, message="The field must be maximium {max} characters")
 	@Email(message = "email invalid")
     private String email;
+    @NotBlank
     @Size(min=11, message="The field must be at least {min} characters")
     @Size(max=11, message="The field must be maximium {max} characters")
     private String cpf;
+    @NotBlank
+    @Size(min=11, message="The field must be at least {min} characters")
+    @Size(max=11, message="The field must be maximium {max} characters")
+    private String password;
     private LocalDateTime dataCriacao;
     private List<Pedido> pedidos = new ArrayList<>();
 
-    public ClienteDTO(){
-    }
+    public List<Pedido> getPedidos() { return pedidos; }
 
-    public ClienteDTO(Cliente cliente){
-        this.id = cliente.getId();
-        this.nome = cliente.getNome();
-        this.email = cliente.getEmail();
-        this.cpf = cliente.getCpf();
-        this.dataCriacao = cliente.getDataCriacao();
-        this.pedidos = cliente.getPedidos();
-    }
+    public Long getId() { return id; }
 
-    public List<Pedido> getPedidos() {
-        return pedidos;
-    }
+    public String getNome() { return nome; }
 
-    public void setPedidos(List<Pedido> pedidos) {
-        this.pedidos = pedidos;
-    }
+    public String getEmail() { return email; }
 
-    public Long getId() {
-        return id;
-    }
+    public String getCpf() { return cpf; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public LocalDateTime getDataCriacao() { return dataCriacao; }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDateTime getDataCriacao() {
-        return dataCriacao;
-    }
-
-    public void setdataCriacao(LocalDateTime dataCriacao) {
-        this.dataCriacao = dataCriacao;
-    }
-
+    public String getPassword() { return password; }
 }
