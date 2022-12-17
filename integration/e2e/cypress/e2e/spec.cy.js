b1340001dcb2da0ba4dba580250e31b2d7a54dc3
yagoazedias <yagoazedias@gmail.com>
2022-12-17 13:06:24 -0300

    Updating spec.cy.js

--- integration/e2e/cypress/e2e/spec.cy.js
+++ integration/e2e/cypress/e2e/spec.cy.js
@@ -1,6 +1,20 @@
-describe("Funcionalidade de Login", () => {
+describe("Funcionalidade de Login (Cliente)", () => {
   it('Deve ser possível carregar a página inicial', () => {
     cy.visit('/');
-    cy.contains('Faça login');
+    cy.get("input[type='text']").type("12312312312");
+    cy.get("input[type='password']").type("12345678");
+    cy.get("input[type='submit']").click();
+  })
+})
+
+describe("Funcionalidade de Login (Garçom)", () => {
+  it('Deve ser possível carregar a página inicial', () => {
+    cy.visit('/');
+    cy.get("div[class='login__type-selector__item']").click();
+    cy.get("input[type='text']").type("11111111111");
+    cy.get("input[type='password']").type("12345678");
+    cy.get("input[type='submit']").click();
+    cy.get('html > body > div > div > div > div > div > div:nth-of-type(1)')
+        .should('have.css', 'cursor','not-allowed')
   })
 })
