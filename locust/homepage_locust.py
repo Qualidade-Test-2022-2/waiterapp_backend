a4d60c1875006e72ff068c22d7e1d97daa5937a8
Yago Azedias <yagoazedias@gmail.com>
2022-12-18 15:07:02 -0300

    Update homepage_locust.py

--- locust/homepage_locust.py
+++ locust/homepage_locust.py
@@ -30,48 +30,48 @@ class WebsiteUser(HttpUser):
         self.client.get("/polyfills.20a1820151dac3ff.js")
         self.client.get("/main.4f84963720bcbb31.js")
         self.client.get("/786.060bc2b94c417f61.js")
-    #
-    # @task
-    # def api_itens(self):
-    #     self.client.get("/api/itens")
-    #
-    # @task
-    # def api_itens(self):
-    #     current_date = "2022-11-15T23:18:01.234Z"
-    #     self.client.post("/api/itens", {
-    #         "cliente": {
-    #             "id": 3,
-    #             "dataCriacao": "2022-11-15T23:21:37.598Z"
-    #         },
-    #         "items": [
-    #             {
-    #                 "item": {
-    #                     "id": 3,
-    #                     "nome": "Frango com quiabo",
-    #                     "descricao": "A clássica receita com todos aqueles detalhes que fazem a "
-    #                                  "diferença. As coxas e sobrecoxas são braseadas, cozinham com "
-    #                                  "pouco líquido, para que a carne fique úmida e a pele dourada.",
-    #                     "dataCriacao": current_date,
-    #                     "preco": 59.99,
-    #                     "ingredientes": [
-    #                         {
-    #                             "id": 2,
-    #                             "nome": "4 coxas de frango com pele e osso",
-    #                             "descricao": "Sem descrição",
-    #                             "dataCriacao": current_date,
-    #                             "caloria": 500
-    #                         },
-    #                         {
-    #                             "id": 1,
-    #                             "nome": "quiabo (cerca de 20 unidades)",
-    #                             "descricao": "Sem descrição",
-    #                             "dataCriacao": current_date,
-    #                             "caloria": 200
-    #                         }
-    #                     ],
-    #                     "caloriaTotal": 700
-    #                 },
-    #                 "quantidade": 1
-    #             }
-    #         ]
-    #     })
+    
+    @task
+    def api_itens(self):
+        self.client.get("/api/itens")
+    
+    @task
+    def api_itens(self):
+        current_date = "2022-11-15T23:18:01.234Z"
+        self.client.post("/api/itens", {
+            "cliente": {
+                "id": 3,
+                "dataCriacao": "2022-11-15T23:21:37.598Z"
+            },
+            "items": [
+                {
+                    "item": {
+                        "id": 3,
+                        "nome": "Frango com quiabo",
+                        "descricao": "A clássica receita com todos aqueles detalhes que fazem a "
+                                     "diferença. As coxas e sobrecoxas são braseadas, cozinham com "
+                                     "pouco líquido, para que a carne fique úmida e a pele dourada.",
+                        "dataCriacao": current_date,
+                        "preco": 59.99,
+                        "ingredientes": [
+                            {
+                                "id": 2,
+                                "nome": "4 coxas de frango com pele e osso",
+                                "descricao": "Sem descrição",
+                                "dataCriacao": current_date,
+                                "caloria": 500
+                            },
+                            {
+                                "id": 1,
+                                "nome": "quiabo (cerca de 20 unidades)",
+                                "descricao": "Sem descrição",
+                                "dataCriacao": current_date,
+                                "caloria": 200
+                            }
+                        ],
+                        "caloriaTotal": 700
+                    },
+                    "quantidade": 1
+                }
+            ]
+        })
